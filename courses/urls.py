521cbcc7c48b90064955b0632f36985d37795427
Vishwajit123 <vishwajit20bhagat@gmail.com>
2020-12-01 06:17:13 +0530

    fixtures, mixin and braces

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,5 @@
 from django.urls  import path
-from . import views
-
+import courses.views as views
 urlpatterns = [
     path("mine/",views.ManageCourseListView.as_view(),name='manage_course_list'),
     path("create/", views.CourseCreateView.as_view(),name='course_create'),
@@ -11,5 +10,7 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/<id>/',views.ContentCreateUpdateView.as_view(), name='module_content_update'),
     path('content/<int:id>/delete/',views.ContentDeleteView.as_view(), name='module_content_delete'),
     path('module/<int:module_id>/',views.ModuleContentListView.as_view(), name='module_content_list'),
-
+    path('module/order/',views.ModuleOrderView.as_view(), name='module_order'),
+    path('content/order/',views.ContentOrderView.as_view(), name='content_order'),
+   
 ]
